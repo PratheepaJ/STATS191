865be4c2c169bd39a459dcc6b638560b9ddaf97c
Pratheepa Jeganathan <pjeganat@stanford.edu>
2019-12-08 01:38:17 -0800

    push chnages

---
+++ LectureNotes/anova_example.R
@@ -0,0 +1,30 @@
+
+Y11 = c(10,20,30)
+Y12 = c(15,12)
+Y13 = c(10,15,20,25)
+Y21 = c(2,3,4)
+Y22 = c(5,6,7,8,9)
+Y23 = c(1,2,3)
+
+barY11. = mean(Y11); barY11.
+barY12. = mean(Y12); barY12.
+barY13. = mean(Y13); barY13.
+barY21. = mean(Y21); barY21.
+barY22. = mean(Y22); barY22.
+barY23. = mean(Y23); barY23.
+
+barY1. = mean(c(Y11, Y12, Y13)); barY1.
+barY2. = mean(c(Y21, Y22, Y23)); barY2.
+
+barY.1 = mean(c(Y11, Y21)); barY.1
+barY.2 = mean(c(Y12, Y22)); barY.2
+barY.3 = mean(c(Y13, Y23)); barY.3
+
+barY.. = mean(c(Y11, Y12, Y13, Y21, Y22, Y23)); barY..
+
+n11 = length(Y11); n11
+n12 = length(Y12); n12
+n13 = length(Y13); n13
+n21 = length(Y21); n21
+n22 = length(Y22); n22
+n23 = length(Y23); n23
